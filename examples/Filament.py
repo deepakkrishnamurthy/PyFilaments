55f34e061f69b37f387b2f9331549a1c5a10c1ea
Deepak Krishnamurthy <deepak90@stanford.edu>
2019-07-17 14:53:47 -0700

    Time dependent activity

--- examples/Filament.py
+++ examples/Filament.py
@@ -20,10 +20,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import odespy
 import os
+import cmocean
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 
 class activeFilament:
     
-    def __init__(self, dim = 3, Np = 1, radius = 1, b0 = 1, k = 1, kappa = 2, S0 = 0,shape ='line'):
+    def __init__(self, dim = 3, Np = 1, radius = 1, b0 = 1, k = 1, S0 = 0, D0 = 0, shape ='line'):
         
         self.dim = dim
         self.Np = Np
@@ -34,7 +37,7 @@ class activeFilament:
         # Connective spring stiffness
         self.k = k
         # Bending stiffness
-        self.kappa = kappa
+        self.kappa = self.k*self.b0
         
         # Fluid viscosity
         self.eta = 1.0/6
@@ -46,8 +49,11 @@ class activeFilament:
         # Initial shape of the filament
         self.shape = shape
         
-        # Stresslet-strength
+        # Stresslet strength
         self.S0 = S0
+        
+        # Potential-Dipole strength
+        self.D0 = D0
         
         # Initiate positions, orientations, forces etc of the particles
         self.r = np.zeros(self.Np*self.dim)
@@ -63,6 +69,22 @@ class activeFilament:
         self.T = np.zeros(self.Np*self.dim)
         # Stresslet 
         self.S = np.zeros(5*self.Np)
+        
+        # Masks for specifying different activities on particles
+        # Mask for external forces
+        self.F_mag = np.zeros(self.Np*self.dim)
+        # Stresslets
+        self.S_mag = np.ones(self.Np)
+        # Potential dipoles
+        self.D_mag = np.zeros(self.Np)
+        
+        # The most distal particle is motile
+#        self.D_mag[-1] = self.D0
+        
+        # All particle have a stresslet strength of S0
+        self.S_mag = self.S_mag*self.S0
+        
+        
         
         
         # Instantiate the pystokes class
@@ -256,14 +278,34 @@ class activeFilament:
                     self.F_conn[j+self.Np] -= fy 
                     self.F_conn[j+xx] -= fz 
                     
+#    def initializeActivity(self, F_mask, S_mask, D_mask):
+#        # Set the initial activity patter
+#        
+#        
+#          
+#    def setActivity(self, t):
+##        Apply a specific activity pattern at time t to the filament
+                    
+    def setForces(self):
+        self.F = self.F_mag
+        
+        
     def setStresslet(self):
         
-        self.S[:self.Np]            = self.S0*(self.p[:self.Np]*self.p[:self.Np] - 1./3)
-        self.S[self.Np:2*self.Np]   = self.S0*(self.p[self.Np:2*self.Np]*self.p[self.Np:2*self.Np] - 1./3)
-        self.S[2*self.Np:3*self.Np] = self.S0*(self.p[:self.Np]*self.p[self.Np:2*self.Np])
-        self.S[3*self.Np:4*self.Np] = self.S0*(self.p[:self.Np]*self.p[2*self.Np:3*self.Np])
-        self.S[4*self.Np:5*self.Np] = self.S0*(self.p[self.Np:2*self.Np]*self.p[2*self.Np:3*self.Np])
-                 
+        self.S[:self.Np]            = self.S_mag*(self.p[:self.Np]*self.p[:self.Np] - 1./3)
+        self.S[self.Np:2*self.Np]   = self.S_mag*(self.p[self.Np:2*self.Np]*self.p[self.Np:2*self.Np] - 1./3)
+        self.S[2*self.Np:3*self.Np] = self.S_mag*(self.p[:self.Np]*self.p[self.Np:2*self.Np])
+        self.S[3*self.Np:4*self.Np] = self.S_mag*(self.p[:self.Np]*self.p[2*self.Np:3*self.Np])
+        self.S[4*self.Np:5*self.Np] = self.S_mag*(self.p[self.Np:2*self.Np]*self.p[2*self.Np:3*self.Np])
+    
+    def setPotDipole(self):
+        
+        # Potential dipole axis is along the local orientation vector of the particles
+        self.D[:self.Np] = self.D_mag*self.p[:self.Np]
+        self.D[self.Np:2*self.Np] = self.D_mag*self.p[self.Np:2*self.Np]
+        self.D[2*self.Np:3*self.Np] = self.D_mag*self.p[2*self.Np:3*self.Np]
+
+              
     def initializeAll(self, filament_shape = 'arc'):
         
         
@@ -291,6 +333,14 @@ class activeFilament:
                     self.r0[ii + self.Np] = self.r0[ii-1 + self.Np] + self.b0*np.sin(ii*arc_angle_piece)
                     
                 
+        elif(filament_shape == 'sinusoid'):
+            nWaves = 2.5
+            Amp=0.01
+            for ii in range(self.Np):
+                # The filament is initially linear along x-axis with the first particle at origin
+                self.r0[ii] = ii*(self.b0)
+                self.r0[ii + self.Np] = Amp*np.sin(nWaves*self.r0[ii]*2*np.pi/((self.Np-1)*self.b0))
+            
             
             
         
@@ -346,12 +396,12 @@ class activeFilament:
         self.rm.stokesletV(self.drdt, self.r, self.F)
         
         # Stressslet contribution to Rigid-Body-Motion
-#        self.rm.stressletV(self.drdt, self.r, self.S)
+        self.rm.stressletV(self.drdt, self.r, self.S)
         
-        self.rm.potDipoleV(self.drdt, self.r, self.p)
+#        self.rm.potDipoleV(self.drdt, self.r, self.p)
         
         # Apply the boundary condition (1st particle is fixed in space)
-        self.drdt[0], self.drdt[self.Np], self.drdt[self.xx] = 0,0,0 
+#        self.drdt[0], self.drdt[self.Np], self.drdt[self.xx] = 0,0,0 
         
         
     
@@ -401,10 +451,16 @@ class activeFilament:
         fig = plt.figure()
         
         ax = fig.add_subplot(1,1,1)
+#        ax1 = fig.add_axes([0.85,0.05,0.02,0.85])
+#        cax1 = make_axes_locatable(ax).append_axes("right", size="5%", pad=0.05)
+
         
         if(self.R is not None):
             
             TimePts, rest  = np.shape(self.R)
+            
+            COM_array = np.zeros((self.dim, TimePts))
+            COM_Speed = np.zeros(TimePts)
             
             self.x_min = np.min(self.R[:,:self.Np])
             self.x_max = np.max(self.R[:,:self.Np])
@@ -418,25 +474,38 @@ class activeFilament:
                 R = self.R[ii,:]
                 t = self.Time[ii]
                 
-                ax.clear()
+                COM_array[0,ii] = np.nanmean(R[:self.Np])
+                COM_array[1,ii] = np.nanmean(R[self.Np:2*self.Np])
                 
-                ax.scatter(R[:self.Np], R[self.Np:2*self.Np], 200, color = 'b', alpha = 1.0, zorder = 2)
+                if(ii>0):
+                    COM_Speed[ii] = ((COM_array[0,ii] - COM_array[0,ii-1])**2 + (COM_array[1,ii] - COM_array[1,ii-1])**2 + (COM_array[2,ii] - COM_array[2,ii-1])**2)**(1/2)
+                
+                ax.clear()
+                                   
+                ax.scatter(R[:self.Np], R[self.Np:2*self.Np], 100, color = 'b', alpha = 1.0, zorder = 2)
+                
+                im = ax.scatter(COM_array[0,:ii], COM_array[1,:ii], 100, c = COM_Speed[:ii], alpha = 1.0, zorder = 2, cmap = 'viridis')
+                
+#                cbar = fig.colorbar(im, cax = cax1, orientation = 'vertical')
                 ax.plot(R[:self.Np], R[self.Np:2*self.Np], color = 'k', alpha = 0.5, zorder = 1)
                 ax.set_title('Time: {:.2f}'.format(t))
                 
+                ax.set_xlabel('X')
+                ax.set_ylabel('Y')
+#                cbar.set_label('COM speed')
                 ax.axis('equal')
 #                ax.axis([self.x_min - 2*self.radius, self.x_max + 2*self.radius , self.y_min-2*self.radius, self.y_max +2*(self.radius)])
 #                ax.set_xlim([x_min - 2*self.radius, x_max + 2*self.radius])
 #                ax.set_ylim([y_min-2*self.radius, y_max +2*(self.radius)])
                 plt.pause(0.000001)
-                
 #                plt.savefig(os.path.join(self.saveFolder, 'Res_T_{:.2f}_'.format(t)+'.tif'), dpi = 150)
+#                cbar = plt.colorbar(ax)
                 
                 fig.canvas.draw()
 
 
 
-        
+
         
         
         
@@ -463,17 +532,19 @@ F_conn = np.zeros(dim*Np)                 # Forces on the particles
 
 
 
-fil = activeFilament(dim = 3, Np = 20, b0 = 4, k = 1, radius = 1, S0= -2, kappa = 10, shape = 'arc')
+fil = activeFilament(dim = 3, Np = 32, b0 = 4, k = 1, radius = 1, S0= 15, shape = 'sinusoid')
 
 fil.plotFilament(r = fil.r0)
 
-Tf = 200
-Npts = 50
+Tf = 500
+Npts = 500
 fil.simulate(Tf, Npts)
 
 finalPos = fil.R[-1,:]
 
 fil.plotSimResult()
+
+fil.plotFilament(r = finalPos)
 
 
 
