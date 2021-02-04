6d08fab602fb6ea4ee20af9457849ccb994faee6
Deepak <deepak90@stanford.edu>
2021-02-03 16:27:52 -0800

    Minor edits

--- activeFilaments_simulation.py
+++ activeFilaments_simulation.py
@@ -24,7 +24,7 @@ activityFreq = 1.0/activity_timescale
 
 
 # Total simulation time
-Tf = activity_timescale*200
+Tf = activity_timescale*20
 
 
 # activity_timescale = 1000
@@ -48,7 +48,7 @@ plt.figure()
 plt.plot(t_array, activity_profile)
 plt.show()
 
-bc = {0:'clamped', -1:'free'}
+bc = {0:'fixed', -1:'free'}
 
 fil = activeFilament(dim = 3, Np = 32, radius = 1, b0 = 2.1, k = 20, S0 = 0, D0 = 0, bc = bc)
 
